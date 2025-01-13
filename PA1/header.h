8e821d21dcbe28ac3317bbbb1a268ff8912f0ab3
ravensramen <153202736+ravensramen@users.noreply.github.com>
2025-01-12 16:30:49 -0800

    Update header.h

--- PA1/header.h
+++ PA1/header.h
@@ -1,8 +1,10 @@
-//Name:
-//Course:
-//Professor + TA:
-//Assignment:
-//Date:
+//Name: Sydnee Boothby
+//Course: CptS122
+//Professor + TA: Andy O'Fallon, 
+//Assignment: PA1
+//Date: 01/12
+
+//add ifndef
 
 //preprocessor directives
 #define _CRT_SECURE_NO_WARNINGS
@@ -11,7 +13,32 @@
 #include <stdio.h> //printf, scanf, file functions...
 #include <string.h> //string mod functions, strtok, strlen...
 #include <stdbool.h> //for boolean functions
+#include<stdlib.h>
+
+//structs definition
+
+typedef enum sleep
+{
+	NONE = 0, ASLEEP = 1, AWAKE = 2, REALLYAWAKE = 3
+} Sleep;
+
+ 
+typedef struct fitbit {
+	char patient[10];
+	char minute[9];
+	double calories;
+	double distance;
+	unsigned int floors;
+	unsigned int heartRate;
+	unsigned int steps;
+	Sleep sleepLevel;
+
+} FitbitData;
+
 
 //function declarations, descriptions in functions.c
 bool checkSuccess(FILE* input);
 void welcomeStatement(void);
+void exitStatement(void);
+void readData(FILE* fitbit_input); 
+char* targetPatient(FILE* fitbit_input);
