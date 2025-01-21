e2c7c5ba18d4464ab2df525f08ce6511f7ce3984
ravensramen <153202736+ravensramen@users.noreply.github.com>
2025-01-20 18:47:38 -0800

    header.h

--- PA1/header.h
+++ PA1/header.h
@@ -1,13 +1,14 @@
 //Name: Sydnee Boothby
 //Course: CptS122
-//Professor + TA: Andy O'Fallon, 
+//Professor + TA: Andy O'Fallon, Kyle Parker
 //Assignment: PA1
-//Date: 01/12
+// Description: Read csv input of a day's worth of fitbit data. Dedupe and clean through data, record analysis of results to an output csv file.
+//Date: 01/20/2025
 
-//add ifndef
 
 //preprocessor directives
 #define _CRT_SECURE_NO_WARNINGS
+#define MISSING_VALUE -1;
 
 //import libraries for functions
 #include <stdio.h> //printf, scanf, file functions...
@@ -42,3 +43,8 @@ void welcomeStatement(void);
 void exitStatement(void);
 void readData(FILE* fitbit_input); 
 char* targetPatient(FILE* fitbit_input);
+void storeData(FILE* input, FitbitData* fitbitdata, int* i);
+char* my_strtok(char* buffer, char delim); 
+int minuteToIndex(char* minute);
+void computeAverages(FitbitData* fitbitdata, int size, FILE* output);
+void poorSleep(FitbitData* fitbitdata, int size, int* start_index, int* end_index);
